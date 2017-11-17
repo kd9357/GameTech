58423239e853821fb6494b746e1bbed09e1d9ea2
Kyle McCarver <kmccarver8@gmail.com>
2017-11-17 00:34:39 -0600

    working on map gen, need to adjust algorithm

---
+++ SlyFoxDie/Assets/Scripts/MazeDoor.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeDoor : MazePassage {
+
+    public Transform hinge;
+
+    public static Quaternion normalRotation = Quaternion.Euler(0f, -90f, 0f),
+                             mirroredRotation = Quaternion.Euler(0f, 90f, 0f);
+
+    private bool isMirrored;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private MazeDoor OtherSideOfDoor
+    {
+        get
+        {
+            return otherCell.GetEdge(direction.GetOpposite()) as MazeDoor;
+        }
+    }
+
+    public override void Initialize(MazeCell primary, MazeCell other, MazeDirection direction)
+    {
+        base.Initialize(primary, other, direction);
+        if(OtherSideOfDoor != null)
+        {
+            isMirrored = true;
+            hinge.localScale = new Vector3(-1f, 1f, 1f);
+            Vector3 p = hinge.localPosition;
+            p.x = -p.x;
+            hinge.localPosition = p;
+        }
+        for(int i = 0; i < transform.childCount; ++i)
+        {
+            Transform child = transform.GetChild(i);
+            if(child != hinge)
+            {
+                child.GetComponent<Renderer>().material = cell.room.settings.wallMaterial;
+            }
+        }
+    }
+
+    public override void OnPlayerEntered()
+    {
+        OtherSideOfDoor.hinge.localRotation = hinge.localRotation = isMirrored ? mirroredRotation : normalRotation;
+    }
+
+    public override void OnPlayerExited()
+    {
+        OtherSideOfDoor.hinge.localRotation = hinge.localRotation = Quaternion.identity;
+    }
+}
---
+++ SlyFoxDie/Assets/Scripts/MazeRoomSettings.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class MazeRoomSettings {
+
+    public Material floorMaterial, wallMaterial;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
